10f45b7761d3f7882c1fd57a6309cf12d432d050
Kwami <103781982+KwomoA1@users.noreply.github.com>
2024-02-06 22:06:34 +0000

    Revamped html page to make functionality easier

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,15 @@ window.addEventListener('load',pageInit)
 
 function pageInit(){
     // elements 
-    const warmUpTimerButton = document.querySelector('#warmUpTimer');
-    const exerciseTimerButton = document.querySelector('#exerciseTimer')
-    const restTimerButton = document.querySelector('#restTimer');
-    const setsButton = document.querySelector('#setsLabel');
-    const minutesSecondsEditor = document.querySelector('#minutesSecondsForm');
-    const minutesInput = document.querySelector('#minutes');
-    const secondsInput = document.querySelector('#seconds');
+
 
     // Event Listeners
     
 
 }
 
-
-/*
 // Update each timer value 
 function setTotalTime(event){
-    const minutesInput = document.querySelector('#minutes');
-    const secondsInput = document.querySelector('#seconds');
     totalTime = {
         warmUpTimer:{
             minutes:10,
@@ -42,8 +32,4 @@ function setTotalTime(event){
 
         }
     }
-
 }
-
-setTotalTime()
-*/
