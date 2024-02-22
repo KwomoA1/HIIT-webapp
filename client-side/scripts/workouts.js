f833108869bafb4def9a843a8708217cc66e97f1
Kwami <103781982+KwomoA1@users.noreply.github.com>
2024-02-22 15:20:50 +0000

    Create a sever file to store all server content

--- client-side/scripts/workouts.js
+++ client-side/scripts/workouts.js
@@ -1,6 +1,14 @@
-/*
-Removes the hidden class from HTML element 
-*/
-function displayObject(obj){
-    
+function init(){
+    const createButton = document.querySelector('#create-workout');
+    createButton.addEventListener('click',() => {
+        const form = document.querySelector("#workout-form-init");
+        if(form.classList.contains("hidden")){
+            form.classList.remove("hidden");
+        } else{
+            form.classList.add("hidden");
+        }
+    })
 }
+
+
+window.addEventListener('load', init);
