db0c5caea3edf67b4e9ca5d953dc912b5721316c
Kwami <Kwami_Dosserie@outlook.com>
2024-02-23 02:09:22 +0000

    Added new TODO completed hidden element function and included json example for creating workouts

--- client-side/scripts/workouts.js
+++ client-side/scripts/workouts.js
@@ -1,22 +1,40 @@
-
-
 /*
-Displays any element containing the hidden class or hides elements containing the hidden class 
+A workout is considered an object it's attribute being its name and multiple exercises in it
+Each excerise is an object with a property of name, description and duration
+Because there is multiple exercises an array is needed 
+Each user can have multiple workouts therefore an array is needed.
 */
 
-function displayElement(elementId){
-    const hiddenElement = document.querySelector(elementId);
-    if(hiddenElement.classList.contains("hidden")){
-        hiddenElement.classList.remove("hidden");
-    } else {
-        hiddenElement.classList.add("hidden");
-    }
-    
+//TODO: Complete code so once the submit button is pressed input fields are generated for each exercise and stored as json
+function init(){
+    const createWorkoutBtn = document.querySelector('#create-workout');
+    createWorkoutBtn.addEventListener('click',displayElement);
+
+    const workoutFormSubmitBtn = document.querySelector('#submit-workout-form');
+    workoutFormSubmitBtn.addEventListener('click',obtainInfo);
 }
 
-function init(){
-    const createButton = document.querySelector('#create-workout');
-    createButton.addEventListener('click',displayElement('#workout-form-init'));
+
+// Displays the create workout form inputs 
+function displayElement(){
+    const createWorkoutForm = document.querySelector("#workout-form-init");
+    if(createWorkoutForm.classList.contains('hidden')){
+        createWorkoutForm.classList.remove('hidden');
+    }else{
+        createWorkoutForm.classList.add('hidden');
+    }
 }
+
+//Obtains Workout name and number exercises to store 
+function obtainInfo(){
+    const workoutNameInput = document.querySelector('#workout-name');
+    const exerciseNumberInput = document.querySelector('#exercise-number');
+    
+    let workoutName =  workoutNameInput.value;
+    let exerciseNum = exerciseNumberInput.value; 
+    console.log(workoutName);
+    console.log(exerciseNum);
+}
+
 
 window.addEventListener('load', init);
