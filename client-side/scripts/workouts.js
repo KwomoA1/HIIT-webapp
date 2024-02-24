68628ef2ba79654bcfe5739fde9119cba4493c6e
Kwami <Kwami_Dosserie@outlook.com>
2024-02-24 01:14:05 +0000

    Create Workout and exercise objects just need to display it

--- client-side/scripts/workouts.js
+++ client-side/scripts/workouts.js
@@ -7,12 +7,19 @@ Each user can have multiple workouts therefore an array is needed.
 
 //TODO: Complete code so once the submit button is pressed input fields are generated for each exercise and stored as json
 
+
+const workoutObj = {
+    "workout name": "",
+    "exercises": [] 
+}
+
 function init(){
     const createWorkoutBtn = document.querySelector('#create-workout');
     createWorkoutBtn.addEventListener('click',displayElement);
 
     const workoutFormSubmitBtn = document.querySelector('#submit-workout-form');
-    workoutFormSubmitBtn.addEventListener('click',generateExerciseInputs);
+    workoutFormSubmitBtn.addEventListener('click',generateEditButtons);
+    workoutFormSubmitBtn.addEventListener('click',createWorkoutObject);
 }
 
 
@@ -37,8 +44,30 @@ function obtainInfo(){
     console.log(exerciseNum);
 }
 
+// Sends objects arguments in
+const pastObject = createWorkoutObject.bind(workoutObj);
+
+// Create workout object and stores it in json format 
+function createWorkoutObject(workoutObject){
+    const workoutName = document.querySelector('#workout-name');
+    const exerciseNumInput = document.querySelector('#exercise-number');
+
+    workoutObj["workout name"] = workoutName.value;
+
+    for(let index = 0; index < exerciseNumInput.value; index++){
+        const exerciseObj = {
+            "name": `Exercise ${index + 1}`,
+            "description": `This is exercise ${index + 1}`,
+            "duration": 0
+        }
+        workoutObj["exercises"].push(exerciseObj)
+    }
+    console.log(workoutObj["workout name"]);
+    console.log(workoutObj["exercises"]);
+}
+
 // Generate buttons to edit each exercise 
-function generateExerciseInputs(){
+function generateEditButtons(){
     const exerciseNumInput = document.querySelector('#exercise-number');
     const setupWorkoutSection = document.querySelector('#edit-exercises-buttons');
     const createWorkoutBtn = document.querySelector('#create-workout');
@@ -47,12 +76,19 @@ function generateExerciseInputs(){
     createWorkoutBtn.classList.add('hidden');
     workoutForm.classList.add('hidden');
 
+// Creates buttons based on number of exercises and sets the attribute 
     for(let index = 0; index < exerciseNumInput.value; index++){
         const editExerciseBtn = document.createElement('button');
         editExerciseBtn.setAttribute('id',`Exercise${index}-button`);
-        editExerciseBtn.setAttribute('class','wrapper');
+        editExerciseBtn.setAttribute('class','wrapper'); //wrapper is a class element which sets element display to block
         editExerciseBtn.textContent = `Edit exercise ${index + 1}`;
         setupWorkoutSection.appendChild(editExerciseBtn);
+
+        editExerciseBtn.addEventListener('click',clickSpecial);
+    }
+
+    function clickSpecial(){
+        console.log("Hello")
     }
 }
 
