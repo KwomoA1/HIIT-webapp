b8d45a21864e83e7098d5880241c60a681202f68
Kwami <Kwami_Dosserie@outlook.com>
2024-03-12 01:14:26 +0000

    Added event lisenter and function for create workout button | Started work on next btn handler function (Create workout object and moving to next form)

--- client-side/scripts/workouts.js
+++ client-side/scripts/workouts.js
@@ -1,11 +1,41 @@
 const userWorkoutsArray = []
 const createWorkoutBtn = document.querySelector('#createWorkoutBtn');
-const createWorkoutForm = document.querySelector('#createWorkoutForm');
 const formSteps = document.querySelectorAll('[data-step]');
+const nextStepBtn = document.querySelector('#stepOneNextBtn');
+let formIndex = 1;
 
 
-createWorkoutBtn.addEventListener('click',(event)=>{
-    
+createWorkoutBtn.addEventListener('click',()=>{
+    formSteps.forEach((element,index) => {
+        if(element.dataset.step == 1 && formIndex == 1){
+            element.classList.add('active');
+        }
+    });
 });
+
+nextStepBtn.addEventListener('click',nextBtnHandler);
+
+// Group functions associated with next button behaviour 
+function nextBtnHandler(){
+    createWorkoutInstance();
+}
+
+// Creates an object storing workout name submitted by user and exercise objects with default values
+function createWorkoutInstance(){ 
+    const numOfExercisesInput = document.querySelector('#numOfExercises').value;
+    const workoutObj = {
+        'workout name': document.querySelector('#workoutName').value,
+        'exercises':[] 
+    }
+    for(i=0; i<numOfExercisesInput; i++){
+        let exerciseObj ={
+            'name': `exercises ${i+1}`,
+            'description': ``,
+            'duration':0
+        }
+        workoutObj['exercises'].push(exerciseObj)
+    }
+    userWorkoutsArray.push(workoutObj);
+}
 
 
