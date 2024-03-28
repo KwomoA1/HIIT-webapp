cda004399be37fa2faecec542e4b7acbbb605f35
Kwami <103781982+KwomoA1@users.noreply.github.com>
2024-03-28 23:21:18 +0000

    Added basic Javascript function (not populated to obtain UI elements and run application

---
+++ client-side/scripts/index.js
@@ -0,0 +1,17 @@
+
+
+// Stores references to UI objects 
+const ui = {} 
+
+//Obtains references 
+function getHandles(){
+
+}
+
+// calls every function to run application
+function main(){
+
+}
+
+//starts application
+main() 
