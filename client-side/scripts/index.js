0917d3f0f74f8d471b0f711a19139fb4f622d370
Kwami <103781982+KwomoA1@users.noreply.github.com>
2024-03-30 01:30:34 +0000

    Added new TODO, Updated code to display step 1 upon clicking button

--- client-side/scripts/index.js
+++ client-side/scripts/index.js
@@ -1,16 +1,30 @@
-
+//TODO:Function to navigate between steps of the form.
 
 // Stores references to UI objects 
 const ui = {} 
 
-//Obtains references 
-function getHandles(){
-
+function setupUiReferences(){
+    ui.createWrkBtn = document.querySelector('#create-Btn');
+    ui.formSections = document.querySelectorAll('form > section');
+    ui.nextStepBtns = document.querySelectorAll('.nextStepBtns');
+    ui.prevStepBtns = document.querySelectorAll('.prevStepBtns') 
+    ui.nextBtns = document.querySelectorAll('.nextBtns');
+    ui.prevBtns = document.querySelectorAll('.prevBtns')
 }
+
+function setupForm(){
+    for(const section of ui.formSections){
+        if(section.classList.contains('hidden') && section.dataset.step == 1){
+            section.classList.remove('hidden');
+        }
+    }
+}
+
 
 // calls every function to run application
 function main(){
-
+    setupUiReferences();
+    ui.createWrkBtn.addEventListener('click',setupForm);
 }
 
 //starts application
