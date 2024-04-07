6ede3ed6f8e034bc27e7dee7ac9247543b48ba01
Kwami <103781982+KwomoA1@users.noreply.github.com>
2024-04-07 23:01:26 +0100

    Create function to create workout object might need to come back to it

--- client-side/scripts/index.js
+++ client-side/scripts/index.js
@@ -2,14 +2,29 @@
 
 // Stores references to UI objects 
 const ui = {} 
+const formElems = {}
+let workoutObj = {
+    workoutName: '',
+    exercises:[]
+}
 
 function setupUiReferences(){
     ui.createWrkBtn = document.querySelector('#create-Btn');
     ui.formSections = document.querySelectorAll('form > section');
     ui.nextStepBtns = document.querySelectorAll('.nextStepBtns');
-    ui.prevStepBtns = document.querySelectorAll('.prevStepBtns') 
+    ui.prevStepBtns = document.querySelectorAll('.prevStepBtns');
+    ui.numExElem = document.querySelector('#num-exercises');
     ui.nextBtns = document.querySelectorAll('.nextBtns');
-    ui.prevBtns = document.querySelectorAll('.prevBtns')
+    ui.prevBtns = document.querySelectorAll('.prevBtns');
+}
+
+function FormElemReferences(){
+    formElems.wrkName = document.querySelector('#workout-name');
+    formElems.numEx = document.querySelector('#num-exercises');
+    formElems.exName = document.querySelector('#exercise-name');
+    formElems.exDesc = document.querySelector('#exercise-desc');
+    formElems.exDur = document.querySelector('#exercise-dur');
+    formElems.restDur = document.querySelector('#rest-dur');
 }
 
 function setupForm(){
@@ -20,11 +35,26 @@ function setupForm(){
     }
 }
 
+function createWrkObject(){
+    workoutObj.workoutName = formElems.wrkName.textContent; 
+    for(let i = 0; i < formElems.numEx.value; i++){
+        execriseObj = {
+            'exercise name': `exercises ${i+1}`,
+            'exercise description': ``,
+            'exercise duration': ``,
+            'rest duration': ``
+        }
+        workoutObj.exercises.push(exerciseObj)
+    }
+}
+
 
 // calls every function to run application
 function main(){
     setupUiReferences();
     ui.createWrkBtn.addEventListener('click',setupForm);
+    ui.numExElem.addEventListener('input',createWrkObject);
+    console.log(workoutObj);
 }
 
 //starts application
