dc73008402b583cf8d1501ce0cef21bec50c882b
Kwami <103781982+KwomoA1@users.noreply.github.com>
2024-04-11 17:50:19 +0100

    Updated form navigation to be more efficient and readable

--- client-side/scripts/index.js
+++ client-side/scripts/index.js
@@ -3,107 +3,53 @@
 // Stores references to UI objects 
 const ui = {};
 const formElems = {};
-let formIndex;
+
 let workoutObj = {
     workoutName: '',
     exercises:[]
 };
 
-function setupUiReferences(){
+// Obtains handles on UI objects
+function uiHandles(){
     ui.createWrkBtn = document.querySelector('#create-Btn');
-    ui.formSections = document.querySelectorAll('form > section');
-    ui.nextStepBtns = document.querySelectorAll('.nextStepBtns');
-    ui.prevStepBtns = document.querySelectorAll('.prevStepBtns');
-    ui.numExElem = document.querySelector('#num-exercises');
-    ui.nextBtns = document.querySelectorAll('.nextBtns');
-    ui.prevBtns = document.querySelectorAll('.prevBtns');
 }
 
-function FormElemReferences(){
-    formElems.wrkName = document.querySelector('#workout-name');
-    formElems.numEx = document.querySelector('#num-exercises');
-    formElems.exTitle = document.querySelector('#exerTitle');
-    formElems.exName = document.querySelector('#exercise-name');
-    formElems.exDesc = document.querySelector('#exercise-desc');
-    formElems.exDur = document.querySelector('#exercise-dur');
-    formElems.restDur = document.querySelector('#rest-dur');
+// Obtain handles on form element objects 
+function formElemHandles(){
+    formElems.workoutForm = document.querySelector('#workout-form');
+    formElems.formSteps = [...formElems.workoutForm.querySelectorAll('.formSection')];
 }
 
-function setupForm(){
-    formIndex = 1;
-    for(const section of ui.formSections){
-        if(section.dataset.step == formIndex){
-            section.classList.add('active');
-        } 
+// Finds the step which contains the active class and displays it 
+// ? Chaining operator returns undefined and prevents type error if no class contains the active class
+function formStepHandler(){
+
+    let currentStep = parseInt(
+        formElems.formSteps.find((step)=>{
+            return step.classList.contains('active')
+    })?.dataset.step); 
+
+    if(isNaN(currentStep)){ //Checks if the value is NaN and sets it to 0 
+        currentStep = 1;
+        displayCurrentStep(currentStep);
     }
+
+    formElems.workoutForm.addEventListener('click',event=>{
+        if(event.target.classList.contains('nextStepBtns')){
+            currentStep += 1;
+            displayCurrentStep(currentStep);
+        } else if (event.target.classList.contains('prevStepBtns')){
+            currentStep -=1;
+            displayCurrentStep(currentStep);
+        }
+    })
+    
 }
 
-function formNavigation(event){
-    for(let i =0; i < ui.nextStepBtns.length; i++){
-        ui.nextStepBtns[i].addEventListener('click', function(event){
-            if(formIndex < 3){
-                formIndex += 1;
-                ui.formSections.forEach(section=>{
-                    section.classList.remove('active');
-                    if(section.dataset.step == formIndex){
-                        section.classList.add('active');
-                    }
-                })
-            }
-
-            if(event.target.value=="submit"){
-                createWrkObject()
-                ui.formSections.forEach(section=>{
-                    if(section.dataset.step ==2){
-                        exerciseNavigation();
-                    }
-                })
-            }
-        })
-    }
-
-    for(let i=0; i < ui.prevStepBtns.length; i++){
-        ui.prevStepBtns[i].addEventListener('click',function(){
-            if(formIndex > 1){
-                formIndex -=1;
-                ui.formSections.forEach(section=>{
-                    section.classList.remove('active');
-                    if(section.dataset.step == formIndex){
-                        section.classList.add('active');
-                    }
-                })
-            }
-        })
-    }
-}
-
-function exerciseNavigation(){
-    const form2Section = document.querySelector('#exercise-form');
-    let exerciseIndex = parseInt(form2Section.dataset.workoutindex);
-    console.log(exerciseIndex)
-    formElems.exTitle.textContent = ` ${workoutObj.exercises[exerciseIndex].exerciseName}`;
-
-    for(i=0; i < ui.nextBtns.length; i++){
-        ui.nextBtns[i].addEventListener('click',function(){
-            if(exerciseIndex < workoutObj.exercises.length-1){
-                exerciseIndex += 1;
-                formElems.exTitle.textContent = `${workoutObj.exercises[exerciseIndex].exerciseName}`;
-            } 
-            
-        })
-    }
-
-    for(i=0; i < ui.prevBtns.length; i++){
-        ui.prevBtns[i].addEventListener('click',function(){
-            if(exerciseIndex > 0){
-                exerciseIndex -= 1;
-                formElems.exTitle.textContent = `${workoutObj.exercises[exerciseIndex].exerciseName}`
-            } 
-
-            
-        })
-    }
-
+function displayCurrentStep(currentStep){
+    formElems.formSteps.forEach((step)=>{
+        step.classList.toggle('active',parseInt(step.dataset.step) === currentStep)
+    });
 }
 
 function createWrkObject(){
@@ -122,11 +68,9 @@ function createWrkObject(){
 
 // calls every function to run application
 function main(){
-    FormElemReferences();
-    setupUiReferences();
-    formNavigation();
-    ui.createWrkBtn.addEventListener('click',setupForm);
-    console.log(workoutObj);
+    uiHandles();
+    formElemHandles();
+    ui.createWrkBtn.addEventListener('click',formStepHandler);
 }
 
 //starts application
